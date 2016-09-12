5c9141e02cbf9ad0dc9eeac54ff3745831353d65
Sascha Grunert <Sascha.Grunert@rohde-schwarz.com>
2016-09-12 11:33:05 +0200

    Fixed unit test expection

--- src/lib.rs
+++ src/lib.rs
@@ -440,7 +440,7 @@ fn parse_and_print_log_1() {
     assert_eq!(journal.config.excluded_tags.len(), 0);
     assert!(journal.parse_log("HEAD", "rc", &0, &true, &false).is_ok());
     assert_eq!(journal.parse_result.len(), journal.tags.len() + 1);
-    assert_eq!(journal.parse_result[0].1.len(), 1);
+    assert_eq!(journal.parse_result[0].1.len(), 4);
     assert_eq!(journal.parse_result[1].1.len(), 1);
     assert_eq!(journal.parse_result[2].1.len(), 2);
     assert!(journal.print_log(false).is_ok());
